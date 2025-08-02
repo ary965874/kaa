b35b46fc9c784659f9bcff41dc6eb0c95b6c73e4
ary965874 <lillijorres59@gmail.com>
2025-08-02 14:45:40 +0530

    Update home.js

--- api/home.js
+++ api/home.js
@@ -3,24 +3,28 @@ import * as cheerio from 'cheerio';
 
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://hdhub4u.build/');
+    const response = await fetch('https://hdhub4u.build');
     const html = await response.text();
     const $ = cheerio.load(html);
 
     const posts = [];
 
-    $('div.blogbox').each((_, el) => {
-      const link = $(el).find('a').attr('href');
+    $('.img_box').each((i, el) => {
       const img = $(el).find('img').attr('src');
-      const title = $(el).find('h2').text().trim();
+      const title = $(el).find('img').attr('alt');
+      const postUrl = $(el).parent('a').attr('href');
 
-      if (link && img && title) {
-        posts.push({ title, image: img, link });
+      if (img && title && postUrl) {
+        posts.push({
+          title: title.trim(),
+          image: img,
+          url: postUrl
+        });
       }
     });
 
     res.status(200).json({ posts });
-  } catch (err) {
-    res.status(500).json({ error: 'Scraping failed', details: err.message });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to scrape home page' });
   }
 }
