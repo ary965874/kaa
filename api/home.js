8037d01271ac5f7c0996a88396ddbcd6a55d0629
ary965874 <lillijorres59@gmail.com>
2025-08-02 15:01:14 +0530

    Update home.js

--- api/home.js
+++ api/home.js
@@ -2,25 +2,31 @@ import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
 
 export default async function handler(req, res) {
+  const url = 'https://hdhub4u.build/';
+
   try {
-    const response = await fetch('https://hdhub4u.build');
+    const response = await fetch(url);
     const html = await response.text();
     const $ = cheerio.load(html);
 
     const posts = [];
 
-    $('article').each((i, el) => {
-      const postUrl = $(el).find('a').first().attr('href');
-      const image = $(el).find('img').first().attr('src');
-      const title = $(el).find('img').first().attr('alt')?.trim();
+    $('li.thumb.col-md-2.col-sm-4.col-xs-6').each((_, el) => {
+      const img = $(el).find('img').attr('src') || '';
+      const link = $(el).find('a').attr('href') || '';
+      const title = $(el).find('figcaption p').text().trim() || '';
 
-      if (postUrl && image && title) {
-        posts.push({ title, image, url: postUrl });
+      if (img && link && title) {
+        posts.push({
+          title,
+          image: img,
+          url: link
+        });
       }
     });
 
     res.status(200).json({ posts });
   } catch (err) {
-    res.status(500).json({ error: 'Failed to scrape home page', details: err.message });
+    res.status(500).json({ error: 'Scraping failed', details: err.message });
   }
 }
